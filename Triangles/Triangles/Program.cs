ebf03da2835ab14fa64cd822cf2a09969c0fc28c
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-05-01 11:28:26 +0300

    chore: add project files

---
+++ Triangles/Triangles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangles
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
